e110cca0ba34ca13043e1ca361a0072cd1071c2d
Your Name <you@example.com>
2025-07-22 20:26:05 +0200

    improved list-devices.py

--- list-devices.py
+++ list-devices.py
@@ -5,6 +5,14 @@
 import evdev
 
 
+something_found = False
+print("Available input devices:")
 for d in evdev.list_devices():
     print(evdev.InputDevice(d).name)
+    something_found = True
+
+
+if not something_found:
+    print("No input devices found. Please connect a device and try again. Ensure you have the necessary permissions to access input devices (e.g., by adding the current user to the \"input\" group).")
+    exit(1)
 
